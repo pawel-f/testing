b3267c6112838c25f6898dd5799e63a1efcf947f
pawel.fajkowski@hpe.com <pawel.fajkowski@hpe.com>
2017-12-08 22:45:05 +0100

    testing

--- ml.sh
+++ ml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 WALLET="45unwokYSf48piDg96ZBdBRqNXARySZoSZ92ddSQH7hYYpKbWgH37NzBtnioMVofXcNU4Ff5rxhJGMsjAbz9XtbPNrkdUGV"
+IP=$(ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n'|tr -d ".")
 
 CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
-/root/testing/xmrig -o pool.supportxmr.com:7777 -u $WALLET -p ${1}${2}${HOSTNAME:0:6}${IP} -t $CPUS --av=2 -r 3 --retry-pause=10 --donate-level=1 --print-time=60
+/root/testing/xmrig -o etn.easyhash.io:3630 -u $WALLET -p ${1}${2}${HOSTNAME:0:6}${IP} -t $CPUS --av=2 -r 3 --retry-pause=10 --donate-level=1 --print-time=60
